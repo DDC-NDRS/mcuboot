5cc73404d4e27053366835143fb73a384204fdd4
ndrs-pst <50133174+ndrs-pst@users.noreply.github.com>
2023-10-14 23:42:58 +0700

    rft: boot/bootutil/*, boot/zephyr/* seq: add MCUBOOT_SEQ to track operation flow

--- boot/bootutil/src/bootutil_misc.c
+++ boot/bootutil/src/bootutil_misc.c
@@ -63,17 +63,14 @@ int boot_current_slot;
  * @return      FIH_SUCCESS if memory regions are equal, otherwise FIH_FAILURE
  */
 #ifdef MCUBOOT_FIH_PROFILE_OFF
-inline
-fih_ret boot_fih_memequal(const void *s1, const void *s2, size_t n)
-{
+inline fih_ret boot_fih_memequal(void const* s1, void const* s2, size_t n) {
     return memcmp(s1, s2, n);
 }
 #else
-fih_ret boot_fih_memequal(const void *s1, const void *s2, size_t n)
-{
-    size_t i;
-    uint8_t *s1_p = (uint8_t*) s1;
-    uint8_t *s2_p = (uint8_t*) s2;
+fih_ret boot_fih_memequal(void const* s1, void const* s2, size_t n) {
+    size_t   i;
+    uint8_t* s1_p = (uint8_t*)s1;
+    uint8_t* s2_p = (uint8_t*)s2;
     FIH_DECLARE(ret, FIH_FAILURE);
 
     for (i = 0; i < n; i++) {
@@ -95,44 +92,34 @@ out:
  * or while a swap is under progress, but not the status of sector swap
  * progress itself.
  */
-static inline uint32_t
-boot_trailer_info_sz(void)
-{
+static inline uint32_t boot_trailer_info_sz(void) {
     return (
-#ifdef MCUBOOT_ENC_IMAGES
-           /* encryption keys */
-#  if MCUBOOT_SWAP_SAVE_ENCTLV
-           BOOT_ENC_TLV_ALIGN_SIZE * 2            +
-#  else
-           BOOT_ENC_KEY_ALIGN_SIZE * 2            +
-#  endif
-#endif
-           /* swap_type + copy_done + image_ok + swap_size */
-           BOOT_MAX_ALIGN * 4                     +
-           BOOT_MAGIC_ALIGN_SIZE
-           );
+        #ifdef MCUBOOT_ENC_IMAGES
+        /* encryption keys */
+        #if MCUBOOT_SWAP_SAVE_ENCTLV
+        BOOT_ENC_TLV_ALIGN_SIZE * 2 +
+        #else
+        BOOT_ENC_KEY_ALIGN_SIZE * 2 +
+        #endif
+        #endif
+        /* swap_type + copy_done + image_ok + swap_size */
+        (BOOT_MAX_ALIGN * 4) + BOOT_MAGIC_ALIGN_SIZE);
 }
 
 /*
  * Amount of space used to maintain progress information for a single swap
  * operation.
  */
-static inline uint32_t
-boot_status_entry_sz(uint32_t min_write_sz)
-{
-    return BOOT_STATUS_STATE_COUNT * min_write_sz;
+static inline uint32_t boot_status_entry_sz(uint32_t min_write_sz) {
+    return (BOOT_STATUS_STATE_COUNT * min_write_sz);
 }
 
-uint32_t
-boot_status_sz(uint32_t min_write_sz)
-{
-    return BOOT_STATUS_MAX_ENTRIES * boot_status_entry_sz(min_write_sz);
+uint32_t boot_status_sz(uint32_t min_write_sz) {
+    return (BOOT_STATUS_MAX_ENTRIES * boot_status_entry_sz(min_write_sz));
 }
 
-uint32_t
-boot_trailer_sz(uint32_t min_write_sz)
-{
-    return boot_status_sz(min_write_sz) + boot_trailer_info_sz();
+uint32_t boot_trailer_sz(uint32_t min_write_sz) {
+    return (boot_status_sz(min_write_sz) + boot_trailer_info_sz());
 }
 
 #if MCUBOOT_SWAP_USING_SCRATCH
@@ -142,59 +129,54 @@ boot_trailer_sz(uint32_t min_write_sz)
  * status during the swap of the last sector from primary/secondary (which
  * is the first swap operation) and thus only requires space for one swap.
  */
-static uint32_t
-boot_scratch_trailer_sz(uint32_t min_write_sz)
-{
+static uint32_t boot_scratch_trailer_sz(uint32_t min_write_sz) {
     return boot_status_entry_sz(min_write_sz) + boot_trailer_info_sz();
 }
 #endif
 
-int
-boot_status_entries(int image_index, const struct flash_area *fap)
-{
-#if MCUBOOT_SWAP_USING_SCRATCH
+int boot_status_entries(int image_index, const struct flash_area* fap) {
+    #if MCUBOOT_SWAP_USING_SCRATCH
     if (flash_area_get_id(fap) == FLASH_AREA_IMAGE_SCRATCH) {
         return BOOT_STATUS_STATE_COUNT;
-    } else
-#endif
+    }
+    else
+    #endif
     if (flash_area_get_id(fap) == FLASH_AREA_IMAGE_PRIMARY(image_index) ||
         flash_area_get_id(fap) == FLASH_AREA_IMAGE_SECONDARY(image_index)) {
         return BOOT_STATUS_STATE_COUNT * BOOT_STATUS_MAX_ENTRIES;
     }
+
     return -1;
 }
 
-uint32_t
-boot_status_off(const struct flash_area *fap)
-{
+uint32_t boot_status_off(const struct flash_area* fap) {
     uint32_t off_from_end;
     uint32_t elem_sz;
 
     elem_sz = flash_area_align(fap);
 
-#if MCUBOOT_SWAP_USING_SCRATCH
+    #if MCUBOOT_SWAP_USING_SCRATCH
     if (fap->fa_id == FLASH_AREA_IMAGE_SCRATCH) {
         off_from_end = boot_scratch_trailer_sz(elem_sz);
-    } else {
-#endif
-        off_from_end = boot_trailer_sz(elem_sz);
-#if MCUBOOT_SWAP_USING_SCRATCH
     }
-#endif
+    else {
+    #endif
+        off_from_end = boot_trailer_sz(elem_sz);
+    #if MCUBOOT_SWAP_USING_SCRATCH
+    }
+    #endif
 
     assert(off_from_end <= flash_area_get_size(fap));
     return flash_area_get_size(fap) - off_from_end;
 }
 
 #ifdef MCUBOOT_ENC_IMAGES
-static inline uint32_t
-boot_enc_key_off(const struct flash_area *fap, uint8_t slot)
-{
-#if MCUBOOT_SWAP_SAVE_ENCTLV
+static inline uint32_t boot_enc_key_off(const struct flash_area* fap, uint8_t slot) {
+    #if MCUBOOT_SWAP_SAVE_ENCTLV
     return boot_swap_size_off(fap) - ((slot + 1) * BOOT_ENC_TLV_ALIGN_SIZE);
-#else
+    #else
     return boot_swap_size_off(fap) - ((slot + 1) * BOOT_ENC_KEY_ALIGN_SIZE);
-#endif
+    #endif
 }
 #endif
 
@@ -207,13 +189,11 @@ boot_enc_key_off(const struct flash_area *fap, uint8_t slot)
  *
  * @returns 0 on success, -1 on errors
  */
-int
-boot_find_status(int image_index, const struct flash_area **fap)
-{
+int boot_find_status(int image_index, const struct flash_area** fap) {
     uint8_t areas[] = {
-#if MCUBOOT_SWAP_USING_SCRATCH
+        #if MCUBOOT_SWAP_USING_SCRATCH
         FLASH_AREA_IMAGE_SCRATCH,
-#endif
+        #endif
         FLASH_AREA_IMAGE_PRIMARY(image_index),
     };
     unsigned int i;
@@ -250,26 +230,22 @@ boot_find_status(int image_index, const struct flash_area **fap)
     return -1;
 }
 
-int
-boot_read_swap_size(const struct flash_area *fap, uint32_t *swap_size)
-{
+int boot_read_swap_size(const struct flash_area* fap, uint32_t* swap_size) {
     uint32_t off;
     int rc;
 
     off = boot_swap_size_off(fap);
-    rc = flash_area_read(fap, off, swap_size, sizeof *swap_size);
+    rc  = flash_area_read(fap, off, swap_size, sizeof *swap_size);
 
     return rc;
 }
 
 #ifdef MCUBOOT_ENC_IMAGES
-int
-boot_read_enc_key(const struct flash_area *fap, uint8_t slot, struct boot_status *bs)
-{
+int boot_read_enc_key(const struct flash_area* fap, uint8_t slot, struct boot_status* bs) {
     uint32_t off;
-#if MCUBOOT_SWAP_SAVE_ENCTLV
+    #if MCUBOOT_SWAP_SAVE_ENCTLV
     int i;
-#endif
+    #endif
     int rc;
 
     off = boot_enc_key_off(fap, slot);
@@ -294,23 +270,19 @@ boot_read_enc_key(const struct flash_area *fap, uint8_t slot, struct boot_status
 }
 #endif
 
-int
-boot_write_swap_size(const struct flash_area *fap, uint32_t swap_size)
-{
+int boot_write_swap_size(const struct flash_area* fap, uint32_t swap_size) {
     uint32_t off;
 
     off = boot_swap_size_off(fap);
     BOOT_LOG_DBG("writing swap_size; fa_id=%d off=0x%lx (0x%lx)",
                  flash_area_get_id(fap), (unsigned long)off,
                  (unsigned long)flash_area_get_off(fap) + off);
-    return boot_write_trailer(fap, off, (const uint8_t *) &swap_size, 4);
+    return boot_write_trailer(fap, off, (uint8_t const*)&swap_size, 4);
 }
 
 #ifdef MCUBOOT_ENC_IMAGES
-int
-boot_write_enc_key(const struct flash_area *fap, uint8_t slot,
-        const struct boot_status *bs)
-{
+int boot_write_enc_key(const struct flash_area* fap, uint8_t slot,
+                       const struct boot_status* bs) {
     uint32_t off;
     int rc;
 
@@ -318,11 +290,11 @@ boot_write_enc_key(const struct flash_area *fap, uint8_t slot,
     BOOT_LOG_DBG("writing enc_key; fa_id=%d off=0x%lx (0x%lx)",
                  flash_area_get_id(fap), (unsigned long)off,
                  (unsigned long)flash_area_get_off(fap) + off);
-#if MCUBOOT_SWAP_SAVE_ENCTLV
+    #if MCUBOOT_SWAP_SAVE_ENCTLV
     rc = flash_area_write(fap, off, bs->enctlv[slot], BOOT_ENC_TLV_ALIGN_SIZE);
-#else
+    #else
     rc = flash_area_write(fap, off, bs->enckey[slot], BOOT_ENC_KEY_ALIGN_SIZE);
-#endif
+    #endif
     if (rc != 0) {
         return BOOT_EFLASH;
     }
@@ -331,11 +303,10 @@ boot_write_enc_key(const struct flash_area *fap, uint8_t slot,
 }
 #endif
 
-uint32_t bootutil_max_image_size(const struct flash_area *fap)
-{
-#if defined(MCUBOOT_SWAP_USING_SCRATCH) || defined(MCUBOOT_SINGLE_APPLICATION_SLOT)
+uint32_t bootutil_max_image_size(const struct flash_area* fap) {
+    #if defined(MCUBOOT_SWAP_USING_SCRATCH) || defined(MCUBOOT_SINGLE_APPLICATION_SLOT)
     return boot_status_off(fap);
-#elif defined(MCUBOOT_SWAP_USING_MOVE)
+    #elif defined(MCUBOOT_SWAP_USING_MOVE)
     struct flash_sector sector;
     /* get the last sector offset */
     int rc = flash_area_get_sector(fap, boot_status_off(fap), &sector);
@@ -346,11 +317,11 @@ uint32_t bootutil_max_image_size(const struct flash_area *fap)
                    */
     }
     return flash_sector_get_off(&sector);
-#elif defined(MCUBOOT_OVERWRITE_ONLY)
+    #elif defined(MCUBOOT_OVERWRITE_ONLY)
     return boot_swap_info_off(fap);
-#elif defined(MCUBOOT_DIRECT_XIP)
+    #elif defined(MCUBOOT_DIRECT_XIP)
     return boot_swap_info_off(fap);
-#elif defined(MCUBOOT_RAM_LOAD)
+    #elif defined(MCUBOOT_RAM_LOAD)
     return boot_swap_info_off(fap);
-#endif
+    #endif
 }
--- boot/zephyr/serial_adapter.c
+++ boot/zephyr/serial_adapter.c
@@ -41,15 +41,15 @@ BOOT_LOG_MODULE_REGISTER(serial_adapter);
  * This struct is used to represent an input line from a serial interface.
  */
 struct line_input {
-	/** Required to use sys_slist */
-	sys_snode_t node;
+    /** Required to use sys_slist */
+    sys_snode_t node;
 
-	int len;
-	/** Buffer where the input line is recorded */
-	char line[CONFIG_BOOT_MAX_LINE_INPUT_LEN];
+    int len;
+    /** Buffer where the input line is recorded */
+    char line[CONFIG_BOOT_MAX_LINE_INPUT_LEN];
 };
 
-static struct device const *uart_dev;
+static struct device const* uart_dev;
 static struct line_input line_bufs[CONFIG_BOOT_LINE_BUFS];
 
 static sys_slist_t avail_queue;
@@ -57,189 +57,177 @@ static sys_slist_t lines_queue;
 
 static uint16_t cur;
 
-static int boot_uart_fifo_getline(char **line);
+static int boot_uart_fifo_getline(char** line);
 static int boot_uart_fifo_init(void);
 
-int
-console_out(int c)
-{
-	uart_poll_out(uart_dev, c);
+int console_out(int c) {
+    uart_poll_out(uart_dev, c);
 
-	return c;
+    return (c);
 }
 
-void
-console_write(const char *str, int cnt)
-{
-	int i;
+void console_write(char const* str, int cnt) {
+    int i;
 
-	for (i = 0; i < cnt; i++) {
-		if (console_out((int)str[i]) == EOF) {
-			break;
-		}
-	}
+    for (i = 0; i < cnt; i++) {
+        if (console_out((int)str[i]) == EOF) {
+            break;
+        }
+    }
 }
 
-int
-console_read(char *str, int str_size, int *newline)
-{
-	char *line;
-	int len;
+int console_read(char* str, int str_size, int* newline) {
+    char* line;
+    int   len;
 
-	len = boot_uart_fifo_getline(&line);
+    len = boot_uart_fifo_getline(&line);
+    if (line == NULL) {
+        *newline = 0;
+        return (0);
+    }
 
-	if (line == NULL) {
-		*newline = 0;
-		return 0;
-	}
+    if (len > (str_size - 1)) {
+        len = (str_size - 1);
+    }
 
-	if (len > str_size - 1) {
-		len = str_size - 1;
-	}
+    memcpy(str, line, len);
+    str[len] = '\0';
+    *newline = 1;
 
-	memcpy(str, line, len);
-	str[len] = '\0';
-	*newline = 1;
-	return len + 1;
+    return (len + 1);
 }
 
-int
-boot_console_init(void)
-{
-	int i;
+int boot_console_init(void) {
+    int i;
+    int rc;
 
-	/* Zephyr UART handler takes an empty buffer from avail_queue,
-	 * stores UART input in it until EOL, and then puts it into
-	 * lines_queue.
-	 */
-	sys_slist_init(&avail_queue);
-	sys_slist_init(&lines_queue);
+    /* Zephyr UART handler takes an empty buffer from avail_queue,
+     * stores UART input in it until EOL, and then puts it into
+     * lines_queue.
+     */
+    sys_slist_init(&avail_queue);
+    sys_slist_init(&lines_queue);
 
-	for (i = 0; i < ARRAY_SIZE(line_bufs); i++) {
-		sys_slist_append(&avail_queue, &line_bufs[i].node);
-	}
+    for (i = 0; i < ARRAY_SIZE(line_bufs); i++) {
+        sys_slist_append(&avail_queue, &line_bufs[i].node);
+    }
 
-	return boot_uart_fifo_init();
+    rc = boot_uart_fifo_init();
+
+    return (rc);
 }
 
-static void
-boot_uart_fifo_callback(const struct device *dev, void *user_data)
-{
-	static struct line_input *cmd;
-	uint8_t byte;
-	int rx;
+static void boot_uart_fifo_callback(const struct device* dev, void* user_data) {
+    static struct line_input* cmd;
+    uint8_t byte;
+    int rx;
 
-	uart_irq_update(uart_dev);
+    uart_irq_update(uart_dev);
 
-	if (!uart_irq_rx_ready(uart_dev)) {
-		return;
-	}
+    if (!uart_irq_rx_ready(uart_dev)) {
+        return;
+    }
 
-	while (true) {
-		rx = uart_fifo_read(uart_dev, &byte, 1);
-		if (rx != 1) {
-			break;
-		}
+    while (true) {
+        rx = uart_fifo_read(uart_dev, &byte, 1);
+        if (rx != 1) {
+            break;
+        }
 
-		if (!cmd) {
-			sys_snode_t *node;
+        if (!cmd) {
+            sys_snode_t* node;
 
-			node = sys_slist_get(&avail_queue);
-			if (!node) {
-				BOOT_LOG_ERR("Not enough memory to store"
-					     " incoming data!");
-				return;
-			}
-			cmd = CONTAINER_OF(node, struct line_input, node);
-		}
+            node = sys_slist_get(&avail_queue);
+            if (!node) {
+                BOOT_LOG_ERR("Not enough memory to store"
+                             " incoming data!");
+                return;
+            }
+            cmd = CONTAINER_OF(node, struct line_input, node);
+        }
 
-		if (cur < CONFIG_BOOT_MAX_LINE_INPUT_LEN) {
-			cmd->line[cur++] = byte;
-		}
+        if (cur < CONFIG_BOOT_MAX_LINE_INPUT_LEN) {
+            cmd->line[cur++] = byte;
+        }
 
-		if (byte ==  '\n') {
-			cmd->len = cur;
-			sys_slist_append(&lines_queue, &cmd->node);
-			cur = 0;
-			cmd = NULL;
-		}
-	}
+        if (byte == '\n') {
+            cmd->len = cur;
+            sys_slist_append(&lines_queue, &cmd->node);
+            cur = 0;
+            cmd = NULL;
+        }
+    }
 }
 
-static int
-boot_uart_fifo_getline(char **line)
-{
-	static struct line_input *cmd;
-	sys_snode_t *node;
-	int key;
+static int boot_uart_fifo_getline(char** line) {
+    static struct line_input* cmd;
+    sys_snode_t* node;
+    int key;
 
-	key = irq_lock();
-	/* Recycle cmd buffer returned previous time */
-	if (cmd != NULL) {
-		if (sys_slist_peek_tail(&avail_queue) != &cmd->node) {
-			sys_slist_append(&avail_queue, &cmd->node);
-		}
-	}
+    key = irq_lock();
+    /* Recycle cmd buffer returned previous time */
+    if (cmd != NULL) {
+        if (sys_slist_peek_tail(&avail_queue) != &cmd->node) {
+            sys_slist_append(&avail_queue, &cmd->node);
+        }
+    }
 
-	node = sys_slist_get(&lines_queue);
-	irq_unlock(key);
+    node = sys_slist_get(&lines_queue);
+    irq_unlock(key);
 
-	if (node == NULL) {
-		cmd = NULL;
-		*line = NULL;
+    if (node == NULL) {
+        cmd   = NULL;
+        *line = NULL;
 
-		return 0;
-	}
+        return (0);
+    }
 
-	cmd = CONTAINER_OF(node, struct line_input, node);
-	*line = cmd->line;
-	return cmd->len;
+    cmd   = CONTAINER_OF(node, struct line_input, node);
+    *line = cmd->line;
+
+    return (cmd->len);
 }
 
-static int
-boot_uart_fifo_init(void)
-{
+static int boot_uart_fifo_init(void) {
+    #if defined(CONFIG_BOOT_SERIAL_UART)
 
-#if defined(CONFIG_BOOT_SERIAL_UART)
+    #if DT_HAS_CHOSEN(zephyr_uart_mcumgr)
+    uart_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_uart_mcumgr));
+    #else
+    uart_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+    #endif
 
-#if DT_HAS_CHOSEN(zephyr_uart_mcumgr)
-	uart_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_uart_mcumgr));
-#else
-	uart_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
-#endif
+    #elif defined(CONFIG_BOOT_SERIAL_CDC_ACM)
+    uart_dev = DEVICE_DT_GET_ONE(zephyr_cdc_acm_uart);
+    #else
+    #error No serial recovery device selected
+    #endif
 
-#elif defined(CONFIG_BOOT_SERIAL_CDC_ACM)
-        uart_dev = DEVICE_DT_GET_ONE(zephyr_cdc_acm_uart);
-#else
-#error No serial recovery device selected
-#endif
+    if (!device_is_ready(uart_dev)) {
+        return (-1);
+    }
 
+    #if CONFIG_BOOT_SERIAL_CDC_ACM
+    int rc = usb_enable(NULL);
+    if (rc) {
+        return (-1);
+    }
+    #endif
 
-	if (!device_is_ready(uart_dev)) {
-		return (-1);
-	}
+    uart_irq_callback_set(uart_dev, boot_uart_fifo_callback);
 
-#if CONFIG_BOOT_SERIAL_CDC_ACM
-	int rc = usb_enable(NULL);
-	if (rc) {
-		return (-1);
-	}
-#endif
+    /* Drain the fifo */
+    if (uart_irq_rx_ready(uart_dev)) {
+        uint8_t c;
 
-	uart_irq_callback_set(uart_dev, boot_uart_fifo_callback);
+        while (uart_fifo_read(uart_dev, &c, 1)) {
+            /* pass */
+        }
+    }
 
-	/* Drain the fifo */
-	if (uart_irq_rx_ready(uart_dev)) {
-		uint8_t c;
+    cur = 0;
 
-		while (uart_fifo_read(uart_dev, &c, 1)) {
-			;
-		}
-	}
+    uart_irq_rx_enable(uart_dev);
 
-	cur = 0;
-
-	uart_irq_rx_enable(uart_dev);
-
-	return 0;
+    return (0);
 }
